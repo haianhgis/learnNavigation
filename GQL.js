c6dd6517637d874ee4bbd0f5bc0756db7250564b
haianhgis <33014106+haianhgis@users.noreply.github.com>
2017-11-17 22:54:06 +0700

    Add files via upload

---
+++ GQL.js
@@ -0,0 +1,60 @@
+
+/*
+HOM NAY, TOI QUYET DINH DAT TEN CHO NO LA JGL (JAVASCRIPT GML LANGUAGE...)
+S = SELECT, D = (DISTINCT,DELETE), U = UPDATE , O = ORDER, W = WHERE.. FC : function callback , J : JOIN, T : TABLE
+    - moi method cua JGL deu viet thuong, tren nguyen tac sieu ngan
+    - fck
+*/
+
+
+// lay huyen trong tinh
+
+var GQL=(function(){
+    var me={};
+    me.getvalue = function(){
+        
+    }
+    me.runsp = function(){
+        
+    }
+    me.getsp = function(){
+
+    }
+    // neu w=null => no where, o = null => no order
+    me.swo = function(tb, col, w, o, fc){
+        
+    }
+    me.sdwo = function(){ // co distinct
+        
+    }
+    me.d2db = function(){
+        
+    }
+    me.u2db = function(){
+        
+    }
+    me.i2db = function(){
+        
+    }
+    me.jl2wo = function(tb1,tb2,w,o){ // join left 2 table
+        
+    }
+    me.ji2wo = function(){ // join inner 2t
+        
+    }
+    me.swo = function(){
+        
+    }
+    me.sdwo_fts= function(){
+
+    }
+    me.swo_fts = function(){
+
+    }
+    me.ftswo(tb,col,w,o, function(){
+        
+    })
+    
+    return me;
+})();
+
